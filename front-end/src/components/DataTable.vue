c0f5e0fba117a0bdb682191ce080fd2abe45379d
WanchengWildMan <1150327966@qq.com>
2021-06-11 20:59:18 +0800

    去掉纵向缩放

--- front-end/src/components/DataTable.vue
+++ front-end/src/components/DataTable.vue
@@ -696,93 +696,7 @@ export default {
     filterHeadDataToForm() {
       return this.tableHeadData.filter((item) => !item.autoAdd);
     },
-    tableResizeInit() {
-      let self = this;
-      /* 获取头部td集合,这边是测试表格，只有一个所以直接el-table__body 的0，后续可以在<el-table> 加class，
-      再用querySelector
-      */
-      let tblObj = document.getElementsByClassName("el-table__body")[0];
-      //如果不用数组，最后遍历的时候不能有【】这个来选取元素
-      let headerRows = new Array();
-      for (let i = 0; i < tblObj.rows.length; i++) {
-        //只有rows这个能选，col要先选rows，然后用cells
-        headerRows[i] = tblObj.rows[i].cells[0];
-      }
-      // 去头部的位置
-      let headerTds = document.getElementsByClassName("el-table__body")[0]
-        .rows[0].cells;
-      let screenYStart = 0;
-      let tdHeight = 0;
-      let headerHeight = 0;
 
-      for (let i = 0; i < headerRows.length; i++) {
-        //添加头部单元格事件
-        this.addListener(headerRows[i], "mousedown", onmousedown);
-        this.addListener(headerRows[i], "mousemove", onmousemove);
-      }
-
-      document.onmousedown = function (event) {
-        if (this.resizeable) {
-          let evt = event || window.event;
-          this.mousedown = true;
-          screenYStart = evt.screenY;
-          tdHeight = this.targetTd.offsetHeight;
-          headerHeight = tblObj.offsetHeight;
-        }
-      };
-      document.onmousemove = function (event) {
-        let evt = event || window.event;
-        let srcObj = self.getTarget(evt);
-        let offsetY = evt.offsetY;
-        if (this.mousedown) {
-          let height = tdHeight + (evt.screenY - screenYStart) + "px"; //计算后的新的宽度，原始td+偏移
-          this.targetTd.style.height = height;
-          tblObj.style.height =
-            headerHeight + (evt.screenY - screenYStart) + "px";
-        } else {
-          if (
-            srcObj.offsetHeight - evt.offsetY <= 8 &&
-            srcObj.offsetWidth - evt.offsetX > 8
-          ) {
-            this.targetTd = srcObj;
-            this.resizeable = true;
-            srcObj.style.cursor = "row-resize"; //修改光标样式
-          } else if (evt.offsetY <= 8 && evt.offsetX > 8) {
-            if (srcObj.parentNode.rowIndex) {
-              this.targetTd =
-                tblObj.rows[srcObj.parentNode.rowIndex - 1].cells[0];
-              this.resizeable = true;
-              srcObj.style.cursor = "row-resize";
-            }
-          } else if (
-            srcObj.offsetHeight - evt.offsetY > 8 &&
-            srcObj.offsetWidth - evt.offsetX <= 8
-          ) {
-            srcObj.style.cursor = "column-resize"; //修改光标样式
-          } else if (evt.offsetY > 8 && evt.offsetX <= 8) {
-            if (srcObj.parentNode.rowIndex) {
-              srcObj.style.cursor = "column-resize";
-            }
-          } else {
-            this.resizeable = false;
-            srcObj.style.cursor = "default";
-          }
-        }
-      };
-      //放开鼠标恢复原位
-      document.onmouseup = function (event) {
-        this.tartgetTd = null;
-        this.coltargetTd = null;
-        this.resizeable = false;
-        this.mousedown = false;
-        document.body.style.cursor = "default";
-      };
-    },
-    // 得到目标值事件
-    getTarget(evt) {
-      return evt.target || evt.srcElement;
-    },
-    // 添加监听
     addListener(element, type, listener, useCapture) {
       //这是两种写法，对应不同浏览器
       element.addEventListener
@@ -947,6 +861,10 @@ export default {
             !this.searchStr ||
             reg.test(item.articleName) ||
             reg.test(item.author) || //.join(",")) ||
+              reg.test(item.publishYear)||
+              reg.test(item.id)||
+              reg.test(item.publishHouse)||
+              reg.test(item.articleType)||
             // reg.test(item.author.join(" ")) ||
             reg.test(item.articleType)
           );
